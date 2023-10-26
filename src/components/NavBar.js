d777d370d948519cac13a6638f387cfe63442e3c
Santabarbara Jolicoeur <jolicoeur.s@northeastern.edu>
2023-10-26 08:30:29 -0400

    navbar done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
-import {Navbar, Container} from "react-bootstrap";
+import {Navbar, Container, Nav} from "react-bootstrap";
 import logo from '../assets/img/logo.svg'
-import logo from '../assets/img/logo.svg'
-import logo from '../assets/img/logo.svg'
-import logo from '../assets/img/logo.svg'
+import navIcon1 from '../assets/img/nav-icon1.svg'
+import navIcon2 from '../assets/img/nav-icon2.svg'
+import navIcon3 from '../assets/img/nav-icon3.svg'
+
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home'); //keep track of what links are clicked
-    const [scolled, setScrolled] = useState(false); //keep track if the screen has scrolled 
+    const [scrolled, setScrolled] = useState(false); //keep track if the screen has scrolled 
 
     useEffect(() => {
         const onScroll = () => {
@@ -30,8 +31,8 @@ export const NavBar = () => {
     return (
         <Navbar expand="lg" className = {scrolled ? "scrolled": ""}> 
           <Container>
-            <Navbar.Brand href="#home">
-                <img src="" alt= "LOGO" />
+            <Navbar.Brand href="/">
+                <img src={logo} alt= "Logo" />
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav"> 
                 <span className="navbar-toggler-icon"></span>
@@ -44,11 +45,11 @@ export const NavBar = () => {
               </Nav>
               <span className="navbar-text">
                     <div className="social-icon">
-                        <a href="#"><img src={} alt= ""/></a>
-                        <a href="#"><img src={} alt= ""/></a>
-                        <a href="#"><img src={} alt= ""/></a>
+                        <a href="#"><img src={navIcon1} alt= ""/></a>
+                        <a href="#"><img src={navIcon2} alt= ""/></a>
+                        <a href="#"><img src={navIcon3} alt= ""/></a>
                     </div>
-                    <button className="vvd" onClick={() => console.log('connect')}></button><span> Let's Connect</span>
+                    <button className="vvd" onClick={() => console.log('connect')}><span> Let's Connect</span></button>
               </span>
             </Navbar.Collapse>
           </Container>
