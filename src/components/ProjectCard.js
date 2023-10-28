cc02b1db4afe09a7067e0b1c7c1fb531317d45c6
Santabarbara Jolicoeur <jolicoeur.s@northeastern.edu>
2023-10-28 04:48:40 -0400

    project modals in progress

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,6 +1,13 @@
-import { Col } from "react-bootstrap"
+import { useState } from "react";
+import { Col } from "react-bootstrap";
+import { ProjectModal } from "./ProjectModal";
+
 
 export const ProjectCard = ({title, description, imgUrl}) => {
+  
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
     return (
       <Col size={12} sm={6} md={4}>
@@ -9,7 +16,12 @@ export const ProjectCard = ({title, description, imgUrl}) => {
           <div className="proj-txtx">
             <h4>{title}</h4>
             <span>{description}</span>
+            <span></span>
+            <button type="button" onClick={handleShow}>
+              <span>Gallery</span>
+            </button>
           </div>
+          <ProjectModal show={show} handleClose={handleClose} />
         </div>
       </Col>
     );
---
+++ src/components/ProjectModal.js
@@ -0,0 +1,24 @@
+
+import { Modal, Row, Col, Carousel} from "react-bootstrap";
+import { ProjectCarousel } from "./ProjectCarousel";
+
+export const ProjectModal = ({ show, handleClose }) => {
+
+    return(
+        <>
+        <Modal size="xl" centered show={show} onHide={handleClose} animation={false}>
+          <Modal.Header closeButton>
+            <Modal.Title className="modaltitle"><h2>Modal heading</h2></Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Row className="modalbody">
+              <ProjectCarousel/>
+                <Col xs={6} md={3} className="projectinfo">
+                    <p>Tech Stack</p>
+                </Col>
+            </Row>
+          </Modal.Body>
+        </Modal>
+      </>
+    )
+}
