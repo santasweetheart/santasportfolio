72c96ac725aa228b210ad6e87f63275461b48d5a
Santabarbara Jolicoeur <jolicoeur.s@northeastern.edu>
2023-10-28 05:19:44 -0400

    modal so far

--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -9,13 +9,14 @@ export const ProjectModal = ({ show, handleClose }) => {
         <>
         <Modal size="xl" centered show={show} onHide={handleClose} animation={false}>
           <Modal.Header closeButton>
-            <Modal.Title className="modaltitle"><h2>Modal heading</h2></Modal.Title>
+            <Modal.Title className="modaltitle"><h2>Project Name</h2></Modal.Title>
           </Modal.Header>
           <Modal.Body>
             <Row className="modalbody">
               <ProjectCarousel/>
                 <Col xs={6} md={3} className="projectinfo">
-                    <p>Tech Stack</p>
+                    <p>Tech Stack</p> 
+                    <button type="button">Github</button>  /** this button should only appear if there is a code base so if that feild is ture or false*/
                 </Col>
             </Row>
           </Modal.Body>
