ec91a4a9e4364937b7d0e6a78778d0016398d0a3
Santabarbara Jolicoeur <jolicoeur.s@northeastern.edu>
2023-10-31 19:40:30 -0400

    removed instagram icon and added links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,6 @@ import { Container, Row, Col } from "react-bootstrap";
 import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const Footer = () => {
   return (
@@ -14,9 +13,8 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+                <a href="www.linkedin.com/in/santajolicoeur" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="LinkedIn Icon" /></a>
+                <a href="https://github.com/santasweetheart"  target="_blank" rel="noopener noreferrer"><img src={navIcon2} alt="Github Icon" /></a>
             </div>
           </Col>
         </Row>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,6 @@ import {Navbar, Container, Nav} from "react-bootstrap";
 import logo from '../assets/img/logo.svg'
 import navIcon1 from '../assets/img/nav-icon1.svg'
 import navIcon2 from '../assets/img/nav-icon2.svg'
-import navIcon3 from '../assets/img/nav-icon3.svg'
 import { HashLink } from 'react-router-hash-link';
 import { BrowserRouter as Router} from "react-router-dom";
 
@@ -48,9 +47,8 @@ export const NavBar = () => {
                 </Nav>
                 <span className="navbar-text">
                       <div className="social-icon">
-                          <a href="#"><img src={navIcon1} alt= ""/></a>
-                          <a href="#"><img src={navIcon2} alt= ""/></a>
-                          <a href="#"><img src={navIcon3} alt= ""/></a>
+                      <a href="www.linkedin.com/in/santajolicoeur" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="LinkedIn Icon" /></a>
+                      <a href="https://github.com/santasweetheart"  target="_blank" rel="noopener noreferrer"><img src={navIcon2} alt="Github Icon" /></a>
                       </div>
                       <HashLink to='#connect'>
                         <button className="vvd"><span>Let’s Connect</span></button>
