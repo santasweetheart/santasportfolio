d0970fd353dfb845e790fce9095405ff96c1cb29
Santabarbara Jolicoeur <jolicoeur.s@northeastern.edu>
2023-11-14 12:36:41 -0500

    Added quote to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,11 @@ export const Footer = () => {
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
-          <Col size={12} sm={6}>
+          <Col size={12} sm={4}>
             <img src={logo} alt="Logo" />
           </Col>
-          <Col size={12} sm={6} className="text-center text-sm-end">
+          <Col size={12} sm={4}><p>"With her Afro and Brown skin,not even the sky was the limit"</p></Col>
+          <Col size={12} sm={4} className="text-center text-sm-end">
             <div className="social-icon">
                 <a href="https://www.linkedin.com/in/santajolicoeur/" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="LinkedIn Icon" /></a>
                 <a href="https://github.com/santasweetheart"  target="_blank" rel="noopener noreferrer"><img src={navIcon2} alt="Github Icon" /></a>
