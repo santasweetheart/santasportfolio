78f065d601d6ca33f76f90daa74635b70671bbd8
Santa Jolicoeur <santa@Santas-Mac-mini.local>
2023-12-10 12:04:21 -0500

    changed doc link

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -44,6 +44,11 @@ export const Banner = () => {
             setDelta(500);
         }
     }
+
+    const openResume = () => {
+        window.open("https://drive.google.com/file/d/1wESlGAERLYZ7yud1hz-kI6CpybEaaR-v/preview", '_blank');
+    }
+    
     
     return(
         <section className="banner" id="home">
@@ -54,7 +59,7 @@ export const Banner = () => {
                     <span className="tagline">Hi! Welcome to my portfolio :)</span>
                     <h1>{"My name is Santa. "} <br/><span className="txt-rotate"> <span className="wrap">{text}</span></span></h1>
                     <p>A usability enthusiast and tech-obsessed nerd. Building and designing human-centered digital experiences is where I shine. Currently, I'm on the lookout for new opportunities where I can contribute, grow, and continue to turn creative ideas into reality.</p>
-                    <a href="https://drive.google.com/file/d/1fUlc6HNkd9doMpCJcMKoJ-WnzBuoLqrR/view?usp=sharing" target="_blank" rel="noreferrer" className="no-underline"><button>My Resume <ArrowRightCircle size={25}/></button></a>
+                    <button onClick={openResume}>My Resume <ArrowRightCircle size={25}/></button>
                 </Col>
                 <Col xs={12} md={6} xl={5}>
                     <img src={headerImg} alt="Header Img"/>
